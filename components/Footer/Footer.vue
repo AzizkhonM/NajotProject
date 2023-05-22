96e06ba5d299dfa43c1d12b2be1de9dc0b7a0624
Azizxon <muzaffarovazizkhon@gmail.com>
2023-05-22 22:38:14 +0500

    Project finished

---
+++ components/Footer/Footer.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="bg-[#F9F8F7]">
+        <div class="max-w-[1182px] m-auto grid grid-cols-4 pt-[30px] gap-x-[20px] border-b border-[#81755C] border-opacity-40">
+            <div>
+                <img src="https://najottalim.uz/_next/static/media/logo-img.d33f4712.svg" class="w-[220px] mb-[25px]" alt="">
+                <h1 class="text-[#607987] font-semibold text-[15px] mb-[20px]">Onlayn platformaga tegishli barcha materiallar "NAJOT TA‘LIM MARKAZI" MCHJning mulki hisoblanadi. Ushbu materiallardan onlayn ta‘lim olishdan tashqari nusxa ko‘chirish, tarqatish va boshqa maqsadlarda foydalanish qonunan man etiladi.</h1>
+            </div>
+            <div>
+                <h1 class="mt-[12px] text-[21px] font-bold mb-[40px]">Ma'lumotlar</h1>
+                <h2 class="mb-[13px] text-[15px] text-[#607987] font-semibold">FAQ</h2>
+                <h2 class="mb-[13px] text-[15px] text-[#607987] font-semibold">Blog</h2>
+                <h2 class="mb-[13px] text-[15px] text-[#607987] font-semibold">Ommaviy oferta</h2>
+            </div>
+            <div>
+                <h1 class="mt-[12px] text-[21px] font-bold mb-[40px]">Ommabop kurslar</h1>
+                <h2 class="mb-[13px] text-[15px] text-[#607987] font-semibold">Dasturlash foundation</h2>
+                <h2 class="mb-[13px] text-[15px] text-[#607987] font-semibold">.NET mutaxassislik kursi</h2>
+                <h2 class="mb-[13px] text-[15px] text-[#607987] font-semibold">SMM</h2>
+            </div>
+            <div>
+                <h1 class="mt-[12px] text-[21px] font-bold mb-[40px]">Bizning loyihalar</h1>
+                <h2 class="mb-[13px] text-[15px] text-[#607987] font-semibold">Osmondagi bolalar</h2>
+                <h2 class="mb-[13px] text-[15px] text-[#607987] font-semibold">0 dan 1 gacha</h2>
+                <h2 class="mb-[13px] text-[15px] text-[#607987] font-semibold">Abutech</h2>
+                <h2 class="mb-[13px] text-[15px] text-[#607987] font-semibold">Alohida mavzu</h2>
+            </div>
+        </div>
+        <div class="pb-[40px] max-w-[1182px] m-auto grid grid-cols-4 pt-[30px] ">
+            <div>
+                <h1 class="text-[15px] font-bold text-[#607987]">Ⓒ Najot Ta'lim, {{ new Date().getFullYear() }}</h1>
+                <h2 class="text-[15px] font-semibold text-[#607987]">Barcha huquqlar himoyalangan.</h2>
+            </div>
+            <div>
+                <h1 class="font-bold text-[17px] mb-[20px]">Toshkent shahri, Chilonzor-9, Qatortol ko‘chasi</h1>
+                <h2 class="text-[15px] font-semibold text-[#607987]">Mo‘ljal: Rayhon milliy taomlari qarshi tomoni, 1- va 2-uylar orqa tomoni.</h2>
+            </div>
+            <div>
+                <h1 class="font-bold text-[17px] mb-[20px]">Tarmoqlar:</h1>
+                <div class="flex"><a href="https://t.me/najottalim"><i class='bx bxl-telegram p-[6px] bg-[#BA8D5B] rounded-full text-white mx-[6px] hover:bg-[#0088cc] duration-300'></i></a><a href="https://www.facebook.com/najottalim"><i class='bx bxl-facebook p-[6px] bg-[#BA8D5B] rounded-full text-white mx-[6px] hover:bg-[#3b5998] duration-300' ></i></a><a href="https://www.youtube.com/c/najottalim"><i class='bx bxl-youtube p-[6px] bg-[#BA8D5B] rounded-full text-white mx-[6px] hover:bg-[#c4302b] duration-300' ></i></a><a href="https://www.instagram.com/najottalim/"><i class='bx bxl-instagram instagram p-[6px] bg-[#BA8D5B] rounded-full text-white mx-[6px] hover:bg-[#962fbf] duration-300' ></i></a><a href="https://www.linkedin.com/company/najottalim/"><i class='bx bxl-linkedin p-[6px] bg-[#BA8D5B] rounded-full text-white mx-[6px] hover:bg-[#0072b1] duration-300' ></i></a></div>
+            </div>
+            <div>
+                <h1 class="font-bold text-[17px] mb-[20px]">Telefon raqami</h1>
+                <a href="tel:+998788889888" class="text-[20px] font-bold text-[#607987]">+998 (78) 888 9 888</a>
+            </div>
+        </div>
+    </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    head: {
-        link: {
-            rel: 'stylesheet',
-            href: 'https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap'
-        }
-    },
     modules: ['@nuxtjs/tailwindcss']
 })
