164bb32fcd1fd6c303c5b49f61910143ed219452
Azizxon <muzaffarovazizkhon@gmail.com>
2023-05-22 22:41:05 +0500

    Project finished

--- components/Sixth.vue
+++ components/Sixth.vue
@@ -11,9 +11,9 @@
                 <div class="px-[45px] py-[33px] bg-white">
                     <h1 class="text-[26px] font-semibold mb-[18px]">Yordam kerakmi?</h1>
                     <h2 class="text-[#607987] font-medium text-[18px] mb-[34px]"><span class="text-[#BA8D5B]">FAQ</span> boʻlimi ham sizga yordam bera olmadimi? Telefon raqamingizni yozib qoldiring, biz sizga qoʻngʻiroq qilamiz va birorta ham savolingiz javobsiz qolmasligiga harakat qilamiz.</h2>
-                    <p class="opacity-40 mb-[5px]">Ismingizni kiriting</p>
+                    <p class="mb-[5px] text-[#607987]">Ismingizni kiriting</p>
                     <input class="w-full rounded-full border py-[10px] px-[15px] text-[19px] mb-[10px]" placeholder="Azizxon" type="text">
-                    <p class="opacity-40 mb-[5px]">Telefon raqamingizni kiriting</p>
+                    <p class="mb-[5px] text-[#607987]">Telefon raqamingizni kiriting</p>
                     <input class="w-full rounded-full border py-[10px] px-[15px] text-[19px] mb-[10px]" value="+998 " type="text">
                     <h1 class="font-semibold text-[15px] mb-[20px]">"So‘rov yuborish" tugmasini bosish orqali <span class="text-[#BA8D5B]">Platformadan foydalanish qoidalari</span>ga rozilik bildirasiz.</h1>
                     <button class="w-full bg-[#BA8D5B] text-[20px] py-[12px] text-white rounded-full border hover:bg-white hover:text-black hover:border-[#BA8D5B] duration-300">So‘rov yuborish</button>
